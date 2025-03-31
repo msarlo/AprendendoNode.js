c79de75f4a70df7d725076821da9e5be569178f1
msarlo <mateussarlo1@outlook.com>
2025-03-31 20:10:50 -0300

    create: criando a rota para postar fotos

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import Fastify from 'fastify';
-import { DbMemory } from './db-memory';
+import { DbMemory } from './db-memory.js';
 
 const server = Fastify();
+
+const banco = new DbMemory()
 
 server.get('/', async (request, reply) => {
     return 'testeando';
@@ -12,7 +14,12 @@ server.get('/fotos', () => {
 })
 
 server.post('/fotos', () => {
-    return 'Ver fotos'
+    banco.create({
+        título: 'foto teste',
+        descrição: 'primeira foto do banco',
+    })
+    
+    console.log(banco.list())
 })
 
 server.put('/fotos/:id', () => {
