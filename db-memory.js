52d2f20ce1d9703baf37d16b9bbd5f7fb91ada01
msarlo <mateussarlo1@outlook.com>
2025-04-02 01:16:52 -0300

    feat: adicionado a opção de pesquisa por palavra chave

--- db-memory.js
+++ db-memory.js
@@ -3,8 +3,22 @@ import { randomUUID } from "crypto";
 export class DbMemory{
     #fotos = new Map();
 
-    list(){
-        return Array.from(this.#fotos.values());
+    list(search){
+        return Array.from(this.#fotos.entries())
+        .map((foto ) =>{
+            const id = fotoArray[0];
+            const data = fotoArray[1];
+            
+            return {
+                id,
+                ... data,
+            }
+        })
+        .filter(foto =>{
+            if(search){
+                return foto.descricao.includes(search)
+            }
+        })
     }
 
     create(foto){
